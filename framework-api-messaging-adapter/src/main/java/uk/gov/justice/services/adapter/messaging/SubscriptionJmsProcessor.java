bc59a195b071621d453e8ad658958be02b3772c3
Mohamed Farouk <mohamed.farouk@hmcts.net>
2018-04-16 16:29:17 +0100

    add subscription manager

--- framework-api-messaging-adapter/src/main/java/uk/gov/justice/services/adapter/messaging/SubscriptionJmsProcessor.java
+++ framework-api-messaging-adapter/src/main/java/uk/gov/justice/services/adapter/messaging/SubscriptionJmsProcessor.java
@@ -1,19 +1,15 @@
 package uk.gov.justice.services.adapter.messaging;
 
-import uk.gov.justice.services.core.interceptor.InterceptorContext;
-
-import java.util.function.Consumer;
-
 import javax.jms.Message;
 
-public interface JmsProcessor {
+public interface SubscriptionJmsProcessor {
 
     /**
      * Process an incoming JMS message by validating the message and then passing the envelope
      * converted from the message to the given consumer.
      *
-     * @param consumer a consumer for the envelope
+     * @param subscriptionManager a subscription manager
      * @param message  a message to be processed
      */
-    void process(final Consumer<InterceptorContext> consumer, final Message message);
+    void process(final ISubscriptionManager subscriptionManager, final Message message);
 }
