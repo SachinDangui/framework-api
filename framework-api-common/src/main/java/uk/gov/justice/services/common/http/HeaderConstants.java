6d6fe7fa941c4c299650d031cb9e96a4f363a87a
vijay Dabas <vijay.dabas@agilesphere.co.uk>
2018-04-20 12:07:26 +0100

    Header contants updated for authLevel

--- framework-api-common/src/main/java/uk/gov/justice/services/common/http/HeaderConstants.java
+++ framework-api-common/src/main/java/uk/gov/justice/services/common/http/HeaderConstants.java
@@ -4,6 +4,7 @@ public final class HeaderConstants {
 
     public static final String ID = "CPPID";
     public static final String USER_ID = "CJSCPPUID";
+    public static final String AUTH_LEVEL = "authLevel";
     public static final String SESSION_ID = "CPPSID";
     public static final String NAME = "CPPNAME";
     public static final String CAUSATION = "CPPCAUSATION";
